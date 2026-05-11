09a3bbcc307f75ba93703123ee3d430f1fca5f97
kwak0715 <kwak0715@gmail.com>
2026-05-11 12:46:17 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const lottoNumbersDiv = document.getElementById('lotto-numbers');
+const generateBtn = document.getElementById('generate-btn');
+
+generateBtn.addEventListener('click', () => {
+  lottoNumbersDiv.innerHTML = '';
+  const numbers = new Set();
+  while(numbers.size < 6) {
+    numbers.add(Math.floor(Math.random() * 45) + 1);
+  }
+  const sortedNumbers = Array.from(numbers).sort((a,b) => a-b);
+  sortedNumbers.forEach(number => {
+    const numberDiv = document.createElement('div');
+    numberDiv.classList.add('lotto-number');
+    numberDiv.textContent = number;
+    lottoNumbersDiv.appendChild(numberDiv);
+  });
+});
